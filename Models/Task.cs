d6626fc6baf3c785328e792279bf42570b424ebf
martinpercu <97993590+martinpercu@users.noreply.github.com>
2025-08-15 14:51:29 -0400

    minimal data annotations to understand

--- Models/Task.cs
+++ Models/Task.cs
@@ -1,19 +1,31 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace apiEF.Models;
 
 public class Task
 {
+    [Key]
     public Guid TaskId { get; set; }
+
+    [ForeignKey("CategoryId")]
     public Guid CategoryId { get; set; }
+
+    [Required]
+    [MaxLength(210)]
     public string Title { get; set; }
     public string Description { get; set; }
     public Priority PriorityTask { get; set; }
     public DateTime DateCreation { get; set; }
     public virtual Category Category { get; set; }
+
+    [NotMapped]
+    public string Summary { get; set; }
 }
 
 public enum Priority
 {
     Low,
     Medium,
-    High   
+    High
 }
