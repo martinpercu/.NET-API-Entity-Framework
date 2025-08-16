275c485135be2b860e9c271b1892986394a01b47
martinpercu <97993590+martinpercu@users.noreply.github.com>
2025-08-16 13:23:41 -0400

    creating example first migration

--- Models/Category.cs
+++ Models/Category.cs
@@ -12,4 +12,5 @@ public class Category
     public string Name { get; set; }
     public string Description { get; set; }
     public virtual ICollection<Task> Tasks { get; set; }
+    public int Relevance { get; set; }
 }
